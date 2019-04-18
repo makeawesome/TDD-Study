9585bfbb4e8511586402f3f3669a75d3ef1a8eeb
Patrick Min <makereallyawesome@gmail.com>
2019-04-18 21:24:48 +0900

    Ch.1 ~ Ch.5

---
+++ WyCash/src/test/money/MoneyTest.java
@@ -0,0 +1,33 @@
+package test.money;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.money.Dollar;
+import main.money.Franc;
+
+public class MoneyTest {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		assertEquals(new Dollar(10), five.times(2));
+		assertEquals(new Dollar(15), five.times(3));
+	}
+	
+	@Test
+	public void testFrancMultiplication() {
+		Franc five = new Franc(5);
+		assertEquals(new Franc(10), five.times(2));
+		assertEquals(new Franc(15), five.times(3));
+	}
+	
+	
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
+	}
+	
+}
