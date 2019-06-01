929d25602a5c63f3440abe7240e7941aed3ba8ef
Patrick Min <makereallyawesome@gmail.com>
2019-06-01 15:27:24 +0900

    ~Ch.14 통화 변환 기능 구현 리팩토링 시 실수한 달러-달러 통화변환 문제를 분리하기 위한 테스트 작성 테스트 코드 없이 helper 클래스인 Pair 생성

--- WyCash/src/main/money/Bank.java
+++ WyCash/src/main/money/Bank.java
@@ -1,9 +1,42 @@
 package main.money;
 
-public class Bank {
-	
-	public Money reduce(Expression source,String to) {
-		return source.reduce(to);
-	}
+import java.util.Hashtable;
 
+public class Bank {
+	private Hashtable<Pair, Integer> rates = new Hashtable<>();
+	
+	public Money reduce(Expression source, String to) {
+		return source.reduce(this, to);
+	}
+	
+	public void addRate(String from, String to, int rate) {
+		rates.put(new Pair(from, to), new Integer(rate));
+	}
+	
+	public int rate(String from, String to) {
+		if(from.equals(to))
+			return 1;
+		
+		Integer rate = (Integer) rates.get(new Pair(from, to)); 
+		return rate.intValue();
+	}
+}
+
+class Pair {
+	public String from;
+	public String to;
+	
+	public Pair(String from, String to) {
+		this.from = from;
+		this.to = to;
+	}
+	
+	public boolean equals(Object object) {
+		Pair pair = (Pair) object;
+		return from.equals(pair.from) && to.equals(pair.to);
+	}
+	
+	public int hashCode() {
+		return 0;
+	}
 }
--- study/todo.md
+++ study/todo.md
@@ -5,8 +5,8 @@
 + ~~Dollar/Franc 중복~~
 + ~~testFrancMultiplication 제거~~
 + 단위가 다른 화폐 덧셈($5 + 10CHF = $10(환율이 2:1일 경우))
-+ 단위가 같은 화폐 덧셈($5 + $5 = $10)
++ ~~단위가 같은 화폐 덧셈($5 + $5 = $10)~~
 + $5 + $5에서 Money 반환하기
 + ~~Bank.reduce(Money)~~
-+ Money에 대한 통화 변환을 수행하는 Reduce
-+ Reduce(Bank, String) 
++ ~~Money에 대한 통화 변환을 수행하는 Reduce~~
++ ~~Reduce(Bank, String)~~
